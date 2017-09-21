fdf18d451528497dfeda9a496c219fc45b18ad8d
jideas <jideas@163.com>
2017-09-21 11:31:18 +0800

    客户端提交测试

--- src/main/java/org/inchain/queue/impl/InchainFQueue.java
+++ src/main/java/org/inchain/queue/impl/InchainFQueue.java
@@ -5,6 +5,7 @@ import net.apexes.fqueue.exception.FileFormatException;
 import org.inchain.queue.PersistentQueue;
 import org.inchain.queue.util.SerializeUtils;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -69,12 +70,27 @@ public class InchainFQueue<T> extends PersistentQueue<T> {
         this.queue.clear();
         try {
             this.queue.close();
+            File file = new File(this.queueName);
+            this.deleteFile(file);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (FileFormatException e) {
             e.printStackTrace();
         }
-        //TODO 删除磁盘文件
+    }
+
+    private void deleteFile(File file) {
+        if (null != file && file.exists()) {//判断文件是否存在
+            if (file.isFile()) {//判断是否是文件
+                file.delete();//删除文件
+            } else if (file.isDirectory()) {//否则如果它是一个目录
+                File[] files = file.listFiles();//声明目录下所有的文件 files[];
+                for (File f : files) {//遍历目录下所有的文件
+                    this.deleteFile(f);//把每个文件用这个方法进行迭代
+                }
+                file.delete();//删除文件夹
+            }
+        }
     }
 
 }
